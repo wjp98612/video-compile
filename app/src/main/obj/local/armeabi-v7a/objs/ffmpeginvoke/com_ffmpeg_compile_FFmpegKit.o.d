76d8650e562464861bdb62101b2fdbcbf1d3099d
wjp <wjp@wjp.com>
2020-12-29 10:15:51 +0800

    media

---
+++ app/src/main/obj/local/armeabi-v7a/objs/ffmpeginvoke/com_ffmpeg_compile_FFmpegKit.o.d
@@ -0,0 +1,112 @@
+/usr/local/studio-work/compile/app/src/main/obj/local/armeabi-v7a/objs/ffmpeginvoke/com_ffmpeg_compile_FFmpegKit.o: \
+  com_ffmpeg_compile_FFmpegKit.c com_ffmpeg_compile_FFmpegKit.h ffmpeg.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/config.h cmdutils.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavcodec/avcodec.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/samplefmt.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/avutil.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/common.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/attributes.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/macros.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/version.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/avconfig.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/mem.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/error.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/rational.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/mathematics.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/intfloat.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/log.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/pixfmt.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/buffer.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/cpu.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/channel_layout.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/dict.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/frame.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavcodec/version.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavfilter/avfilter.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavfilter/version.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavformat/avformat.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavformat/avio.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavformat/version.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libswscale/swscale.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libswscale/version.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/eval.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/fifo.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/threadmessage.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libswresample/swresample.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libswresample/version.h logjam.h
+
+com_ffmpeg_compile_FFmpegKit.h:
+
+ffmpeg.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/config.h:
+
+cmdutils.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavcodec/avcodec.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/samplefmt.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/avutil.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/common.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/attributes.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/macros.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/version.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/avconfig.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/mem.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/error.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/rational.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/mathematics.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/intfloat.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/log.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/pixfmt.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/buffer.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/cpu.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/channel_layout.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/dict.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/frame.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavcodec/version.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavfilter/avfilter.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavfilter/version.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavformat/avformat.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavformat/avio.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavformat/version.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libswscale/swscale.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libswscale/version.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/eval.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/fifo.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/threadmessage.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libswresample/swresample.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libswresample/version.h:
+
+logjam.h:
---
+++ app/src/main/obj/local/armeabi-v7a/objs/ffmpeginvoke/ffmpeg_filter.o.d
@@ -0,0 +1,137 @@
+/usr/local/studio-work/compile/app/src/main/obj/local/armeabi-v7a/objs/ffmpeginvoke/ffmpeg_filter.o: \
+  ffmpeg_filter.c ffmpeg.h /Users/weiweijinpeng/ffmpeg-3.2.4/config.h \
+  cmdutils.h /Users/weiweijinpeng/ffmpeg-3.2.4/libavcodec/avcodec.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/samplefmt.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/avutil.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/common.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/attributes.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/macros.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/version.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/avconfig.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/mem.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/error.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/rational.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/mathematics.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/intfloat.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/log.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/pixfmt.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/buffer.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/cpu.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/channel_layout.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/dict.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/frame.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavcodec/version.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavfilter/avfilter.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavfilter/version.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavformat/avformat.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavformat/avio.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavformat/version.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libswscale/swscale.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libswscale/version.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/eval.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/fifo.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/threadmessage.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libswresample/swresample.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libswresample/version.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavfilter/buffersink.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavresample/avresample.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavresample/version.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/avassert.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/avstring.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/bprint.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/display.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/opt.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/pixdesc.h \
+  /Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/imgutils.h
+
+ffmpeg.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/config.h:
+
+cmdutils.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavcodec/avcodec.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/samplefmt.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/avutil.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/common.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/attributes.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/macros.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/version.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/avconfig.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/mem.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/error.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/rational.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/mathematics.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/intfloat.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/log.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/pixfmt.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/buffer.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/cpu.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/channel_layout.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/dict.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/frame.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavcodec/version.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavfilter/avfilter.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavfilter/version.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavformat/avformat.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavformat/avio.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavformat/version.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libswscale/swscale.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libswscale/version.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/eval.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/fifo.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/threadmessage.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libswresample/swresample.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libswresample/version.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavfilter/buffersink.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavresample/avresample.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavresample/version.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/avassert.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/avstring.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/bprint.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/display.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/opt.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/pixdesc.h:
+
+/Users/weiweijinpeng/ffmpeg-3.2.4/libavutil/imgutils.h:
