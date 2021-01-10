725259efcc80b0fbe3a00637d63444b286888f77
wjp <wjp@wjp.com>
2021-01-10 18:23:21 +0800

    add video process

--- iris/src/main/java/me/add1/iris/ClickType.java
+++
@@ -1,7 +0,0 @@
-package me.add1.iris;
-
-public class ClickType {
-    public static final String HOLDER ="holder";
-    public static final String FORCE_ACTIVE = "force-active";
-    public static final String REQUEST_ACTIVE = "request-active";
-}
--- iris/src/main/java/me/add1/iris/ErrorCode.java
+++
@@ -1,5 +0,0 @@
-package me.add1.iris;
-
-public class ErrorCode {
-   public static final int CACHE_NOT_FOUND = 0x10;
-}
